3bcc9dd504558bf3d64d18fd1a5bbaaa113ae88b
Artem Pakhomov <setnemo@gmail.com>
2023-11-22 14:09:57 +0200

    refactoring

--- resources/views/layouts/users_block.blade.php
+++ resources/views/layouts/users_block.blade.php
@@ -1,9 +1,9 @@
 @php /** @var \Illuminate\Support\Collection|\App\Models\User[] $users */ @endphp
 
-@forelse($users->all() as $it => $user)
+@forelse($users->all()->filter() as $it => $user)
     @if(1 === (1 + $it) % 3)
         <div class="col-lg-12">
-            <div class="row">
+            <div class="row">`
     @endif
                 <div class="col-md-4">
                     <div class="card mb-4">
