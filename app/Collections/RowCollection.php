2d54ba347933134ee53ba40bbd03352235b35efa
Artem Pakhomov <setnemo@gmail.com>
2023-11-22 14:42:21 +0200

    refactoring

--- app/Collections/RowCollection.php
+++ app/Collections/RowCollection.php
@@ -8,11 +8,25 @@ use App\DTOs\Row;
 use Illuminate\Support\Collection;
 
 /**
- * @method array|Row[] all()
  * @property array|Row[] $items
  */
 class RowCollection extends Collection
 {
+    /**
+     * @return array|Row[]
+     */
+    public function all(): array
+    {
+        if ($this->items[0] ?? null) {
+            $amount = $this->items[0]->getAmount();
+            if ('Сума' === $amount || 'sum' === $amount) {
+                unset($this->items[0]);
+            }
+        }
+
+        return $this->items;
+    }
+
     public function hasUniqHash(string $getUniqHash): bool
     {
         return (bool)$this->filter(static function(Row $row) use ($getUniqHash) {
