db84083091de65ef2a3bbbbad880c8546de058e0
Artem Pakhomov <setnemo@gmail.com>
2023-11-27 11:53:08 +0200

    rss

--- app/Http/Controllers/DonateController.php
+++ app/Http/Controllers/DonateController.php
@@ -14,7 +14,7 @@ class DonateController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['rss', 'index']);
     }
 
     public function index()
