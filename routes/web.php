4fa9f16c380c0858768f844d65ec9e6482ba2701
Artem Pakhomov <setnemo@gmail.com>
2023-12-16 19:01:46 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logou
 
 Route::get('/zvit', fn() => view('volunteer.zvit', ['volunteers' => Volunteer::all()]))->name('volunteer.all');
 Route::get('/zvit/{volunteer}', [App\Http\Controllers\VolunteerController::class, 'show'])->name('volunteer.show');
-Route::get('/raffles', fn() => view('volunteer.raffles', data: ['volunteers' => Volunteer::whereIn('key', config('app.raffles'))->get()]))->name('raffles');
+Route::get('/raffles', fn() => view('volunteer.raffles', data: ['volunteers' => Volunteer::query()->where('is_enabled', '=', true)->whereNotIn('user_id', [1,3])->get()]))->name('raffles');
 Route::post('/volunteer', [App\Http\Controllers\VolunteerController::class, 'store'])->name('volunteer.create');
 Route::post('/volunteer/avatar', [App\Http\Controllers\VolunteerController::class, 'storeAvatar'])->name('volunteer.avatar');
 Route::post('/volunteer/key', [App\Http\Controllers\VolunteerController::class, 'checkKey'])->name('volunteer.key');
