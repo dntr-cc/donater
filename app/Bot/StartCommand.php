f52ae597d30f624c9e8941a1964c95708ae192f8
akorchyn <artur.yurii.korchynskyi@gmail.com>
2024-01-15 16:43:48 +0200

    added auth param into bot login

--- app/Bot/StartCommand.php
+++ app/Bot/StartCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Bot;
 
+use Illuminate\Support\Facades\Cache;
 use Telegram\Bot\Commands\Command;
 
 class StartCommand extends Command
@@ -20,6 +21,20 @@ class StartCommand extends Command
      */
     public function handle(): void
     {
+        // Check if the start command has been issued with a login code
+        $message = $this->getUpdate()->getMessage();
+        $text = $message->getText();
+        if (!empty($text)) {
+            $isExist = Cache::pull('login:start:' . $text);
+
+            if ($isExist) {
+
+                $from = $message?->getFrom();
+                Cache::set('login:end:' . $text, $from->toJson(), 60);
+                $this->replyWithMessage(['text' => 'Вхід дозволено!']);
+                return;
+            }
+        }
         $this->replyWithMessage(['text' => 'Вітаю! Введіть код з сайту для авторизації ⬇️']);
     }
 }
