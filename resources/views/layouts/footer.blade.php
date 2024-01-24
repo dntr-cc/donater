6b4e867b34f31d9a4bb7ed84de894657fa1d8053
Artem Pakhomov <setnemo@gmail.com>
2024-01-24 02:34:37 +0200

    some changes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,20 @@
 @php use Illuminate\Foundation\Application; @endphp
 @section('footer')
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-4 my-4 border-top">
-        <div class="container">
-            <div class="col-md-12 d-flex">
+        <div class="container d-flex justify-content-between">
+            <div>
+                <span class="mb-9 mb-md-0 text-body-secondary">
+                    ver.0.0.5.{{ config('app.version') }} © 2023 Artem Pakhomov with
+                    <a href="{{ route('volunteers') }}" class="">volunteers</a>.
+                    Work with ❤️ to Ukraine, Laravel {{ Application::VERSION }}, PHP {{ phpversion() }}, Linux Alpine,
+                    Docker, <a target="_blank" href="https://github.com/dntr-cc/donater" class="">GitHub</a> and GitHub Actions
+                </span>
+            </div>
+            <div>
                 <span class="mb-3 mb-md-0 text-body-secondary">
-                    ver.0.0.5.{{ config('app.version') }} © 2023 Artem Pakhomov with <a href="{{ route('volunteers') }}" class="">volunteers</a>. Work with ❤️ to Ukraine, Laravel {{ Application::VERSION }}, PHP {{ phpversion() }}, Linux Alpine, Docker, GitHub Actions</span>
+                    <img alt="GitHub last deploy" src="https://img.shields.io/github/last-commit/dntr-cc/donater/main?style=flat&link=https%3A%2F%2Fgithub.com%2Fdntr-cc%2Fdonater&label=deployed">
+                    <img alt="GitHub License" src="https://img.shields.io/github/license/dntr-cc/donater?cacheSeconds=1&link=https%3A%2F%2Fgithub.com%2Fdntr-cc%2Fdonater%2Fblob%2Fmain%2FLICENSE">
+                </span>
             </div>
         </div>
     </footer>
