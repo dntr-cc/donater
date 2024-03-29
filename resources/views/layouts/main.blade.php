150e0b42df46019e3075abeec16b2646b08d7a92
Artem Pakhomov <setnemo@gmail.com>
2024-03-30 00:26:30 +0200

    fix bug in main.blade

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,7 +1,7 @@
 @section('main')
 <main class="py-4 flex-shrink-0">
     <div class="container">
-        @if (!in_array(request()->route()->getName(), config('app.excluded_breadcrumbs')))
+        @if (!in_array(request()?->route()?->getName(), config('app.excluded_breadcrumbs')))
         <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{ route('welcome') }}">Головна</a></li>
