d81189432125c01729b2f9febee482db93204856
Artem Pakhomov <setnemo@gmail.com>
2026-05-31 18:22:05 +0300

    add funds

--- resources/views/fundraising/index.blade.php
+++ resources/views/fundraising/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base')
 @section('page_title', 'Всі збори сайту donater.com.ua')
 @section('page_description', '')
-@section('breadcrumb-current', 'Призи')
+@section('breadcrumb-current', 'Всі збори')
 @section('content')
 @push('head-scripts')
     @vite(['resources/js/masonry.js'])
@@ -11,27 +11,17 @@
     <div class="container">
         <h2 class="pb-2 border-bottom">Всі збори
             <a href="{{route('fundraisings')}}" class="btn ">
-                <i class="bi bi-gift"></i>
+                <i class="bi bi-list-task"></i>
                 Всі збори
             </a>
             <a href="{{route('fundraisings.opened')}}" class="btn ">
-                <i class="bi bi-gift"></i>
+                <i class="bi bi-list-stars"></i>
                 Відкриті збори
             </a>
             <a href="{{route('fundraisings.deleted')}}" class="btn ">
-                <i class="bi bi-gift-fill"></i>
+                <i class="bi bi-list-check"></i>
                 Видалені збори
             </a>
-{{--            <a href="{{route('prizes.spent')}}" class="btn ">--}}
-{{--                <i class="bi bi-gift-fill"></i>--}}
-{{--                Розіграні призи--}}
-{{--            </a>--}}
-{{--            @auth()--}}
-{{--                <a href="{{route('prize.new')}}" class="btn ">--}}
-{{--                    <i class="bi bi-plus-circle-fill"></i>--}}
-{{--                    Створити--}}
-{{--                </a>--}}
-{{--            @endauth--}}
         </h2>
 
         <div class="row row-cols-1 row-cols-xl-3 row-cols-lg-2 row-cols-md-2 row-cols-sm-1 g-4 masonry-grid">
